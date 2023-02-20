6ed2f006634e8c7d5850ff15964d8dd1551a27a5
AM-ASKY-97 <am.asky97@gmail.com>
2023-02-21 00:10:30 +0530

    I added a filter to the search results as I type

--- frontend-react/src/Pages/View-User/viewUser.js
+++ frontend-react/src/Pages/View-User/viewUser.js
@@ -10,11 +10,37 @@ const ViewUser = () => {
     const [getValue, setValue] = useState([]);
     const [loding, setLoding] = useState([true]);
     const [error_loding, seterrorLoding] = useState([true]);
+    const [query, setQuery] = useState([]);
+    const [filterdata, setFilterData] = useState([]);
+
+    const handleSearch = (event) =>{
+        const getUserData = event.target.value;
+        console.log(getUserData);
+        setQuery(getUserData);
+
+        if(getUserData.length > 0)
+        {
+            const getUserData = event.target.value.toLowerCase();
+            const searchData = getValue.filter(item=> item.firstName.toLowerCase().includes(getUserData));
+            if(searchData.length > 0)
+            {
+                setValue(searchData);
+            }
+            else{
+                setValue([{"firstName" : "No data Record"}])
+            }
+        }
+
+        else{
+            setValue(filterdata);
+        }
+    }
 
     useEffect(() => {
         axios.get('http://127.0.0.1:8000/api/index').then(res => {
             if (res.data.status === 200) {
                 setValue(res.data.user);
+                setFilterData(res.data.user);
                 setLoding(false);
                 seterrorLoding(false);
             }
@@ -86,8 +112,8 @@ const ViewUser = () => {
             <div className="container pt-1">
                 <div className="row">
                     <div className="col-12 col-md-3 pb-3">
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" value={query} placeholder="Search" aria-label="Search" onChange={(e) => handleSearch(e)} />
                         </form>
                     </div>
                     <div className="col-12 col-md-9 text-end"><Link to="/add-user" className="btn btn-success"><i className="fa fa-user" aria-hidden="true"></i> Add New Contact</Link></div>
